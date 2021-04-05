7bf509b5b5f0e3734378effc8e5d0288629314ca
vinoddiwan <65565944+vinoddiwan@users.noreply.github.com>
2021-04-05 15:43:00 +0200

    Update MySQL LIMIT & OFFSET.sql

--- MySQL LIMIT & OFFSET.sql
+++ MySQL LIMIT & OFFSET.sql
@@ -1,1 +1,14 @@
+-- Only 2 records
+SELECT * FROM personal
+LIMIT 2;    
 
+-- where filter
+SELECT * FROM personal
+WHERE city = 'Agra'
+LIMIT 3;    
+
+-- order filter
+SELECT * FROM personal
+WHERE city = 'Agra'
+ORDER BY name
+LIMIT 3; 
